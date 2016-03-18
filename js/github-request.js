3df2843b571f3f649484fd77e7f6909543ecb124
Chris Young <ctopheryoung@gmail.com>
2016-03-18 09:53:30 -0700

    refactor

--- js/github-request.js
+++ js/github-request.js
@@ -2,11 +2,12 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(ghUser){
   $.get('https://api.github.com/users/' + ghUser + '/repos' + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    $('#results').append('<h3>Public Repositories</h3><ul>');
     response.forEach(function(repo){
-      $('#results').append("<p>" + repo.name + "</p>");
+      $('#results').append('<li>' + repo.name + '</li>');
     });
+    $('#results').append('</ul>');
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#results').append(error.responseJSON.message);
   });
 };
