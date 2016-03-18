f3b98313583fa6682b30e9dce999ca1fa2f3d4e9
Chris Young <ctopheryoung@gmail.com>
2016-03-18 11:48:34 -0700

    display user info in user div

--- js/github-request.js
+++ js/github-request.js
@@ -1,6 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(ghUser){
+
+  $.get('https://api.github.com/users/' + ghUser + '?access_token=' + apiKey).then(function(response){
+    $('#user').append('<h3>' + response.name + '</h3><p><a href="' + response.html_url + '" target="_blank">' + response.login + '</a></p><p>Location: ' + response.location + '</p>');
+    console.log(response);
+  }).fail(function(error){
+    $('#user').append(error.responseJSON.message);
+  });
+
   $.get('https://api.github.com/users/' + ghUser + '/repos' + '?access_token=' + apiKey).then(function(response){
     $('#results').append('<h3>Public Repositories</h3><ul>');
     response.forEach(function(repo){
