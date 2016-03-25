782220e4829efed76b76b87c9d6a20e433c701c9
Chris Young <ctopheryoung@gmail.com>
2016-03-25 08:33:45 -0700

    added SCSS styling

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,6 +2,7 @@ var getRepos = require('./../js/github-request.js').getRepos;
 
 $(document).ready(function() {
   $('#gh-form-submit').click(function() {
+    $('#user').empty();
     $('#results').empty();
     var ghUser = $('#gh-user').val();
     getRepos(ghUser);
--- js/github-request.js
+++ js/github-request.js
@@ -3,16 +3,17 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(ghUser){
 
   $.get('https://api.github.com/users/' + ghUser + '?access_token=' + apiKey).then(function(response){
-    $('#user').append('<h3>' + response.name + '</h3><p><a href="' + response.html_url + '" target="_blank">' + response.login + '</a></p><p>Location: ' + response.location + '</p>');
+    $('#user').append('<h3>' + response.name + '</h3><p><a href="' + response.html_url + '" target="_blank">' + response.login + '</a></p><p>Location: ' + response.location + '</p><hr>');
     console.log(response);
   }).fail(function(error){
     $('#user').append(error.responseJSON.message);
   });
 
   $.get('https://api.github.com/users/' + ghUser + '/repos' + '?access_token=' + apiKey).then(function(response){
-    $('#results').append('<h3>Public Repositories</h3><ul>');
+    $('#results').append('<h3 class="title">Public Repositories:</h3>');
     response.forEach(function(repo){
-      $('#results').append('<li>' + repo.name + '</li>');
+      console.log(response);
+      $('#results').append('<h4 class="repo">' + repo.name + '</h4><p>' + repo.description + ' <a href="' + repo.html_url + '">Link</a></p>');
     });
     $('#results').append('</ul>');
   }).fail(function(error){
